ecece22f7288a55611fd5fbb0822d095d43d8a1b
Viviorigi <duong682@gmail.com>
2025-09-07 14:53:57 +0700

    add chat service

---
+++ chat-service/src/main/java/com/duong/chat/service/ConversationService.java
@@ -0,0 +1,57 @@
+package com.duong.chat.service;
+
+import com.duong.chat.dto.request.ConversationRequest;
+import com.duong.chat.dto.response.ConversationResponse;
+import com.duong.chat.entity.Conversation;
+import com.duong.chat.mapper.ConversationMapper;
+import com.duong.chat.repository.ConversationRepository;
+import com.duong.chat.repository.httpclient.ProfileClient;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ConversationService {
+    ConversationRepository conversationRepository;
+    ProfileClient profileClient;
+
+    ConversationMapper conversationMapper;
+
+    public List<ConversationResponse> myConversations() {
+        return null;
+    }
+
+    public ConversationResponse create(ConversationRequest request) {
+        return null;
+    }
+
+    private String generateParticipantHash(List<String> ids) {
+        StringJoiner stringJoiner = new StringJoiner("_");
+        ids.forEach(stringJoiner::add);
+        return stringJoiner.toString();
+    }
+
+    private ConversationResponse toConversationResponse(Conversation conversation) {
+        String currentUserId = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        ConversationResponse conversationResponse = conversationMapper.toConversationResponse(conversation);
+
+        conversation.getParticipants().stream()
+                .filter(participantInfo -> !participantInfo.getUserId().equals(currentUserId))
+                .findFirst().ifPresent(participantInfo -> {
+                    conversationResponse.setConversationName(participantInfo.getUsername());
+                    conversationResponse.setConversationAvatar(participantInfo.getAvatar());
+                });
+
+        return conversationResponse;
+    }
+}
